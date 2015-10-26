28456f88e84d28cf73429d937259c3fec4de9648
Yao Yuan <opal_obsidian@hotmail.com>
2015-10-26 10:43:03 +0000

    added basic map using datamaps

--- client-side/js/main.js
+++ client-side/js/main.js
@@ -1,48 +1,22 @@
 // angular.module("AAW", []);
 
-var map = new Datamap({element: document.getElementById('container')});
+var map = new Datamap({
+  element: document.getElementById("map"),
+  projection: 'mercator',
+  fills: {
+    defaultFill: "#ABDDA4",
+    authorHasTraveledTo: "#fa0fa0"
+  },
+  responsive: true,
+  done: function(datamap) {
+    datamap.svg.selectAll('.datamaps-subunit').on('click', function(geography) {
+        console.log(geography);
+        // alert(geography.properties.name);
+    });
+  }
+});
 
-// var width = 960;
-// var height = 1160;
+d3.select(window).on('resize', function() {
+      map.resize();
+  });
 
-// var svg = d3.select("body").append("svg")
-//     .attr("width", width)
-//     .attr("height", height);
-
-// d3.json("js/world.json", function(err, world){
-//   if(err) console.log(err);
-//   console.log(world);
-
-//   var subunits = topojson.feature(world, world.objects.subunits);
-//   console.log(subunits);
-
-//   var projection = d3.geo.albers()
-//     .center([24.5, -13])
-//     .rotate([4.4, 0])
-//     .parallels([50, 60])
-//     .scale(200)
-//     .translate([width/2, height/2]);
-
-//   var path = d3.geo.path()
-//     .projection(projection);
-
-//   svg.selectAll(".subunits")
-//     .data(topojson.feature(world, world.objects.subunits).features)
-//     .enter()
-//     .append("path")
-//     .attr("class", function(d){ return "subunit" + d.id; })
-//     .attr("d", path)
-
-//   svg.append("path")
-//     .datum(topojson.mesh(world, world.objects.subunits, function(a, b){ return a!==b; }))
-//     .attr("d", path)
-//     .attr("class", "subunit-boundary");
-
-  // svg.append("path")
-  //   .datum(topojson.mesh(uk, uk.objects.subunits, function(a, b){return a===b && a.id === "IRL"; }))
-  //   .attr("d", path)
-  //   .attr("class", "subunit-boundary IRL");
-
-
-
-// });
