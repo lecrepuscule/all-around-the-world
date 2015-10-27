78937494bf07fec95e5a668fc79791fc584e9b50
Yao Yuan <opal_obsidian@hotmail.com>
2015-10-27 10:55:58 +0000

    can display first question onto page

---
+++ client-side/js/controllers/quizController.js
@@ -0,0 +1,20 @@
+angular.module("AAW").controller("QuizController", QuizController);
+
+function QuizController(QuizFactory){
+  var vm = this;
+
+  vm.all = [];
+  vm.currentCount;
+  vm.currentQuestion;
+
+  QuizFactory.getQuizzes()
+  .then(function(response){
+      console.log(response);
+      vm.all = response;
+      vm.currentCount = 0;
+      vm.currentQuestion = vm.all[vm.currentCount]
+      console.log("currentQuestion is: ")
+      console.log(vm.currentQuestion)
+  })
+
+}
