c050b9e0262195dbcc96ac29b6a87efdb1b26cb9
Yao Yuan <opal_obsidian@hotmail.com>
2015-10-27 13:41:13 +0000

    added scope apply method in directive so that Angular can be notified of the listener event

--- client-side/js/directives/mapViewDirective.js
+++ client-side/js/directives/mapViewDirective.js
@@ -2,7 +2,8 @@ angular.module("AAW")
   .directive("map", ["d3Service", "mapService", function(d3Service, mapService){
   var directive = {};
   directive.restrict = "EA";
-  // directive.templateUrl = "_mapView.html";
+  directive.scope = false;
+  directive.template = "<h1>{{ qc.currentQuestion.question }}</h1>" + "<h1>{{ qc.currentQuestion.currentCount }}</h1>";
   directive.link = function(scope, element, attrs) {
     d3Service.d3().then(function(d3) {
       mapService.datamaps().then(function(datamaps){
@@ -24,6 +25,7 @@ angular.module("AAW")
           },
           done: function(datamap) {
             datamap.svg.selectAll('.datamaps-subunit').on('click', function(geography) {
+              scope.$apply(function(){
                 console.log(scope.qc)
                 console.log(scope.qc.currentQuestion.answer)
                 console.log(scope.qc.currentCount)
@@ -33,6 +35,7 @@ angular.module("AAW")
                   scope.qc.currentCount++
                   scope.qc.currentQuestion = scope.qc.all[scope.qc.currentCount]
                 }
+              })
                 // alert(geography.properties.name);
             });
           },
