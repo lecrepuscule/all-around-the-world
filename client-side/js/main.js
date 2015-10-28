be3672c9fc2500f41f8498845fd73ae45e844353
Yao Yuan <opal_obsidian@hotmail.com>
2015-10-28 00:05:46 +0000

    added angular ui router

--- client-side/js/main.js
+++ client-side/js/main.js
@@ -1,10 +1,32 @@
-angular.module("AAW", ["d3", "datamaps", "doowb.angular-pusher"])
+angular.module("AAW", ["ui.router", "d3", "datamaps", "doowb.angular-pusher"])
 .config(['PusherServiceProvider',
   function(PusherServiceProvider) {
     PusherServiceProvider
     .setToken('67fee088609cc79322b9')
     .setOptions({});
   }
-]);
+])
+.config(MainRouter);
+
+
+function MainRouter($stateProvider, $urlRouterProvider){
+  console.log("router linked up")
+  $urlRouterProvider.otherwise("/");
+  // $locationProvider.html5Mode(true);
+
+  $stateProvider
+    .state("home", {
+      url: "/",
+      templateUrl: "home.html"
+    })
+    .state("archive", {
+      url: "/archive",
+      templateUrl: "archive.html"
+    })
+    .state("login", {
+      url: "/login",
+      templateUrl: "login.html"
+    })
+}
 
 
